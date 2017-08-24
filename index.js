afe9bdee66363703c1c27b9575735039e5815bac
Chris Loughney <chris@chrisloughney.com>
2017-08-23 22:21:05 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const WebSocket = require('ws');
-const connectionManager = require('./lib/websockets/connection-manager');
-const registerMopidy = require('./lib/modules/mopidy');
+const { connectionManager } = require('./lib/websockets');
 
 connectionManager.setServer(new WebSocket.Server({ port: 9000 }));
 
-registerMopidy();
+require('./lib/modules/mopidy')();
